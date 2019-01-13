1f10bfdad1d1db324648ebad033820f2f79a4d19
Josh <10765599+meow10811@users.noreply.github.com>
2019-01-12 20:26:38 -0500

    Made MecanumTankTeleOp

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/MecanumTankTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/MecanumTankTeleOp.java
@@ -3,11 +3,11 @@ package org.firstinspires.ftc.teamcode.TeleOp;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 @SuppressWarnings("unused")
-@TeleOp(name = "MecanumArcade TeleOp")
-public class MecanumArcadeTeleOp extends TeleOpDaddy {
+@TeleOp(name = "MecanumTank TeleOp")
+public class MecanumTankTeleOp extends TeleOpDaddy {
     @Override
     public void runOpMode() throws InterruptedException {
-        super.type = Drive.DriveType.MECANUMARCADE;
+        super.type = Drive.DriveType.MECANUMTANK;
         super.runOpMode();
     }
 }
